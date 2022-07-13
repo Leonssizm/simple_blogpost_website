9eb9b480792944c370d01f27005c5c8c1e75dbcb
“Leonssizm” <levani.kereselidze11@gmail.com>
2022-07-13 15:22:11 +0400

    refactor: created function in post.js to avoid code duplication, removed style in index.html and created <h2> in post.html

--- assets/Scripts/post.js
+++ assets/Scripts/post.js
@@ -2,14 +2,16 @@ let params = new URLSearchParams(document.location.search);
 let postId = params.get("postId");
 
 if (postId == "101") {
-  document.getElementById("post-title").innerHTML =
-    localStorage.getItem("title");
-  document.getElementById("post-body").innerHTML = localStorage.getItem("body");
+  setPost(localStorage.getItem("title"), localStorage.getItem("body"));
 } else {
   fetch("https://jsonplaceholder.typicode.com/posts/" + postId)
     .then((response) => response.json())
     .then((posts) => {
-      document.getElementById("post-title").innerHTML = posts.title;
-      document.getElementById("post-body").innerHTML = posts.body;
+      setPost(posts.title, posts.body);
     });
+}
+
+function setPost(title, body) {
+  document.getElementById("post-title").innerHTML = title;
+  document.getElementById("post-body").innerHTML = body;
 }
